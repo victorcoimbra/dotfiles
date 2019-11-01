313cd146d5ac7bdf8b8d1db95dacd3fa94bb8ac5
victor.coimbra@outlook.com <victor.coimbra@outlook.com>
2019-11-01 16:00:11 -0300

    init

--- basics/install.sh
+++ basics/install.sh
@@ -1,15 +1,17 @@
 !/bin/bash
 
+# A collection os applications that I use and sometimes I need to remember 
+
 # Update
 sudo pacman -Sys
 
 # Install yay
 
-sudo pacman -S make fakeroot gcc
-git clone https://aur.archlinux.org/yay.git ~/yay
-cd ~/yay
-makepkg -si
-yay
+# sudo pacman -S make fakeroot gcc
+# git clone https://aur.archlinux.org/yay.git ~/yay
+# cd ~/yay
+# makepkg -si
+# yay
 
 # Install mpd
 # sudo pacman  -S mpd
@@ -22,19 +24,8 @@ yay
 # yay mopidy
 # yay mopidy-spotify
 
-# Install i3
-yay i3
-# sudo pacman -S dmenu
-
 # Install Chrome
 yay chrome
-
-# Install terminator
-sudo pacman -S terminator
-sudo pacman -S python2-requests
-mkdir -p $HOME/.config/terminator/plugins
-wget https://git.io/v5Zww -O $HOME"/.config/terminator/plugins/terminator-themes.py"
-
 
 # Install Polybar
 sudo pacman -S polybar
@@ -45,6 +36,12 @@ cp /usr/share/doc/polybar/config ~/.config/polybar/config
 # Install zsh
 sudo pacman -S zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install Kitty
+sudo pacman -S kitty
+
+# Install Flameshot
+yay flameshot
 
 # Install code
 sudo pacman -S code
